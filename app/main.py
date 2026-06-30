7d10bbca2c55abb8b8eae319de9de17fc10862c2
C Sandeep Aithal <149362264+sandeepaithal@users.noreply.github.com>
2026-06-30 08:48:11 +0530

    added domain to CORS

--- app/main.py
+++ app/main.py
@@ -36,6 +36,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://chatsphere-ivory-nine.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
